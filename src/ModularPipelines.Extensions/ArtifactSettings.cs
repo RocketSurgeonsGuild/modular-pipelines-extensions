79703cf40020ae18054f5a7e81166ff03427710e
David Driscoll <david.driscoll@gmail.com>
2026-06-27 17:40:05 -0400

    Apply suggestions from code review
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- src/ModularPipelines.Extensions/ArtifactSettings.cs
+++ src/ModularPipelines.Extensions/ArtifactSettings.cs
@@ -7,7 +7,7 @@ namespace Rocket.Surgery.ModularPipelines.Extensions;
 [ServiceRegistration(ServiceLifetime.Singleton)]
 public class ArtifactSettings(SharedSettings sharedSettings, IConfiguration configuration)
 {
-    public Folder ArtifactsDirectory => ( field ??= configuration.GetValue<Folder?>("ArtifactsDirectory") ?? sharedSettings.RootDirectory / "artifacts" ).EnsureExists();
+    public Folder ArtifactsDirectory => field ??= ( configuration.GetValue<Folder?>("ArtifactsDirectory") ?? sharedSettings.RootDirectory / "artifacts" ).EnsureExists();
 
-    public Folder LogsDirectory => ( field ??= configuration.GetValue<Folder?>("LogsDirectory") ?? ArtifactsDirectory / "logs" ).EnsureExists();
+    public Folder LogsDirectory => field ??= ( configuration.GetValue<Folder?>("LogsDirectory") ?? ArtifactsDirectory / "logs" ).EnsureExists();
 }

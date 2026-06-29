0daa22d4238d6b657e121f91a94359fd0e0c1711
David Driscoll <david.driscoll@gmail.com>
2026-06-28 23:31:27 -0400

    Remove unused module imports and add DocsSettings and DocsModule for documentation management (#4)

---
+++ src/ModularPipelines.Extensions/DocsSettings.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ModularPipelines.FileSystem;
+
+namespace Rocket.Surgery.ModularPipelines.Extensions;
+
+
+[ServiceRegistration(ServiceLifetime.Singleton)]
+public class DocsSettings(SharedSettings sharedSettings, IConfiguration configuration)
+{
+    public bool DocsEnabled { get; } = configuration.GetValue("Docs:Enabled", true);
+    public Folder DocsDirectory => field ??= ( configuration.GetValue<Folder?>("Docs:Directory") ?? sharedSettings.RootDirectory / "docs" ).EnsureExists();
+    public string DocsBuildTask => field ??= configuration.GetValue<string>("Docs:Task") ?? "docs:build";
+    public Folder DocsOutputDirectory => field ??= configuration.GetValue<Folder?>("Docs:Output") ?? DocsDirectory / "dist";
+}
--- src/ModularPipelines.Extensions/Interception/CommandInterceptorExtensions.cs
+++ src/ModularPipelines.Extensions/Interception/CommandInterceptorExtensions.cs
@@ -1,15 +1,11 @@
 using Microsoft.Extensions.DependencyInjection;
 using ModularPipelines.Options;
-using Rocket.Surgery.ModularPipelines.Extensions.Modules;
 
 namespace Rocket.Surgery.ModularPipelines.Extensions.Interception;
 
 public static class CommandInterceptorExtensions
 {
-    public static IServiceCollection AddCommandInterceptor<T>(this IServiceCollection services) where T : class, ICommandInterceptor
-    {
-        return services.AddScoped<ICommandInterceptor, T>();
-    }
+    public static IServiceCollection AddCommandInterceptor<T>(this IServiceCollection services) where T : class, ICommandInterceptor => services.AddScoped<ICommandInterceptor, T>();
     public static IServiceCollection AddCommandInterceptors<T>(this IServiceCollection services) where T : class
     {
         var items = typeof(T).GetInterfaces()
@@ -17,24 +13,18 @@ public static class CommandInterceptorExtensions
             .ToArray();
         foreach (var item in items)
         {
-            Func<IServiceProvider, T> factory = provider => provider.GetRequiredService<T>();
+            static T factory(IServiceProvider provider) => provider.GetRequiredService<T>();
             services.AddScoped<T>();
-            services.AddScoped(item, factory);
+            services.AddScoped(item, (Func<IServiceProvider, T>)factory);
             services.AddScoped(typeof(ICommandInterceptor), typeof(TypedInterceptor<>).MakeGenericType(item.GetGenericArguments()[0]));
         }
         return services;
     }
 
-    public static IServiceCollection AddCommandInterceptor(this IServiceCollection services, Func<CommandLineToolOptions, CommandExecutionOptions?, CancellationToken, ValueTask<(CommandLineToolOptions Options, CommandExecutionOptions? ExecutionOptions)>> interceptFunc)
-    {
-        return services.AddScoped<ICommandInterceptor>(_ => new InlineInterceptor(interceptFunc));
-    }
+    public static IServiceCollection AddCommandInterceptor(this IServiceCollection services, Func<CommandLineToolOptions, CommandExecutionOptions?, CancellationToken, ValueTask<(CommandLineToolOptions Options, CommandExecutionOptions? ExecutionOptions)>> interceptFunc) => services.AddScoped<ICommandInterceptor>(_ => new InlineInterceptor(interceptFunc));
 
     public static IServiceCollection AddCommandInterceptor<TOptions>(this IServiceCollection services, Func<TOptions, CommandExecutionOptions?, CancellationToken, ValueTask<(CommandLineToolOptions Options, CommandExecutionOptions? ExecutionOptions)>> interceptFunc)
-    where TOptions : CommandLineToolOptions
-    {
-        return services.AddScoped<ICommandInterceptor>(_ => new InlineOptionsCommandInterceptor<TOptions>(interceptFunc));
-    }
+    where TOptions : CommandLineToolOptions => services.AddScoped<ICommandInterceptor>(_ => new InlineOptionsCommandInterceptor<TOptions>(interceptFunc));
 
     private class InlineOptionsCommandInterceptor<TOptions>(Func<TOptions, CommandExecutionOptions?, CancellationToken, ValueTask<(CommandLineToolOptions Options, CommandExecutionOptions? ExecutionOptions)>> interceptFunc) : ICommandInterceptor where TOptions : CommandLineToolOptions
     {

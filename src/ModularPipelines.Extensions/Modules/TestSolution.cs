b05b896cb7f4aac9db04eeb406bfbead1e0093a6
David Driscoll <david.driscoll@gmail.com>
2026-06-30 00:31:15 -0400

    Add TestSettings class for test configuration management

--- src/ModularPipelines.Extensions/Modules/TestSolution.cs
+++ src/ModularPipelines.Extensions/Modules/TestSolution.cs
@@ -1,7 +1,5 @@
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using ModularPipelines.FileSystem;
 using File = ModularPipelines.FileSystem.File;
 
 namespace Rocket.Surgery.ModularPipelines.Extensions.Modules;
@@ -70,13 +68,4 @@ public partial class TestSolution
     }
 
     public record Result(CommandResult CommandResult, File Trx, File Binlog);
-
-    [ServiceRegistration(ServiceLifetime.Singleton)]
-    public class Settings(SharedSettings sharedSettings, ArtifactSettings artifactSettings, IConfiguration configuration)
-    {
-        public bool IsEnabled => configuration.GetValue<bool?>("EnableTests") ?? true;
-        public Folder TestsDirectory => field ??= ( sharedSettings.GetConfigurationFolder(nameof(TestsDirectory)) ?? artifactSettings.LogsDirectory / "tests" ).EnsureExists();
-        public Folder CoverageDirectory => field ??= ( sharedSettings.GetConfigurationFolder(nameof(CoverageDirectory)) ?? artifactSettings.LogsDirectory / "coverage" ).EnsureExists();
-        public File RunSettings => field ??= sharedSettings.GetConfigurationFile(nameof(RunSettings)) ?? ( TestsDirectory + "coverage.runsettings" )!;
-    }
 }
